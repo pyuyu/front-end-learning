7140af2fd6394e9edce8cddbd6b681e0b39f1251
pyuyu <122561112@qq.com>
2019-12-16 20:13:41 +0800

    debounce&&throttle

---
+++ fe-code/debounce&&throttle.js
@@ -0,0 +1,22 @@
+function debounce(fn, wait){
+    let timeout = null
+    return function (){
+        let context = this
+        clearTimeout(timeout)
+        timeout = setTimeout(() => fn.apply.apply(context, arguments), wait)
+    }
+}
+
+function throttle(fn, wait){
+    let timeout = null
+    return function(){
+        context = this
+        if(!timeout){
+            timeout = setTimeout(() => {
+                timeout = null
+                fn.apply(context,arguments)
+            }, wait)
+        }
+    }
+ }
+
--- fe-code/debounce.js
+++
@@ -1,9 +0,0 @@
-function debounce(fn, wait){
-    let timeout = null
-    return function (){
-        let context = this
-        clearTimeout(timeout)
-        timeout = setTimeout(() => fn.apply.apply(context, arguments), wait)
-    }
-}
-
