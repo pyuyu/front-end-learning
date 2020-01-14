5ecfcb33e5172cbef3552139ba4dc54842254e13
pyuyu <122561112@qq.com>
2020-01-14 21:11:12 +0800

    [42] 接雨水

---
+++ leet-code/42.接雨水.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let maxHight = Math.max(...height)
+    let sum = 0
+    for(let i = 1; i < maxHight; i++){
+        let isStart = false; //标记是否开始更新 temp
+        let temp_sum = 0;
+        for (let j = 0; j < height.length; j++) {
+            if (isStart && height[j] < i) {
+                temp_sum++;
+            }
+            if (height[j] >= i) {
+                isStart = true;
+                sum = sum + temp_sum;
+                temp_sum = 0;
+            }
+        }
+
+    }
+    return sum
+};
+// @lc code=end
+
