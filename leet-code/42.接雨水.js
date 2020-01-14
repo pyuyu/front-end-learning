ab08ea18919ffc2f669eb5403481daabf90baf7c
pyuyu <122561112@qq.com>
2020-01-14 22:15:48 +0800

    [42] 接雨水

--- leet-code/42.接雨水.js
+++ leet-code/42.接雨水.js
@@ -12,22 +12,22 @@
 var trap = function(height) {
     let maxHight = Math.max(...height)
     let sum = 0
-    for(let i = 1; i < maxHight; i++){
+    for(let i = 1; i <= maxHight; i++){
         let isStart = false; //标记是否开始更新 temp
         let temp_sum = 0;
         for (let j = 0; j < height.length; j++) {
-            if (isStart && height[j] < i) {
-                temp_sum++;
-            }
             if (height[j] >= i) {
                 isStart = true;
                 sum = sum + temp_sum;
                 temp_sum = 0;
+            }else if (isStart && height[j] < i) {
+                temp_sum++;
             }
+           
         }
-
     }
     return sum
 };
+console.log(trap([2,0,2]))
 // @lc code=end
 
