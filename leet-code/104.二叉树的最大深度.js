0073a0f6ab25e7783f0493cb649a38bfff167bd6
pyuyu <122561112@qq.com>
2020-01-16 20:39:45 +0800

    [104] 二叉树的最大深度

---
+++ leet-code/104.二叉树的最大深度.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root === null) return 0
+    let l = maxDepth(root.left) + 1
+    let r = maxDepth(root.right) + 1
+    return Math.max(l,r)
+};
+// @lc code=end
+
