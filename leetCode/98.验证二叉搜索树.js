e4511c185b397e2a012a87ae10c0618c2822f821
pyuyu <122561112@qq.com>
2021-06-02 00:11:26 +0800

    [98] 验证二叉搜索树

---
+++ leetCode/98.验证二叉搜索树.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+  let stack = []
+  let pre = -Infinity
+  while (stack.length || root) {
+    while (root) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()
+    if (root.val <= pre) return false
+    pre = root.val
+    root = root.right
+  }
+  return true
+};
+// @lc code=end
+
