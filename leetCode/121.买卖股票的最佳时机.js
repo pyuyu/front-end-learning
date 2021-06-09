d21e876f807b7074ce58c590cfab41736d6c8f82
pyuyu <122561112@qq.com>
2021-06-09 23:09:47 +0800

    [121] 买卖股票的最佳时机

---
+++ leetCode/121.买卖股票的最佳时机.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=121 lang=javascript
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let minPrice = Number.MAX_VALUE
+  let maxProfit = 0
+  for (let i = 0; i < prices.length; i++) {
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice)
+    minPrice = Math.min(minPrice, prices[i])
+  }
+  return maxProfit
+};
+// @lc code=end
+
