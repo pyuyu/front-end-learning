4afd8a6fa42b828606a9f2636e8551fd65ffd6d9
pyuyu <122561112@qq.com>
2021-07-21 19:33:34 +0800

    [88] 合并两个有序数组

---
+++ leetCode/88.合并两个有序数组.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=javascript
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1
+  let j = n - 1
+  let k = m + n - 1
+  while (i >= 0 || j >= 0) {
+    if (i < 0) nums1[k--] = nums2[j--]
+    else if (j < 0) nums1[k--] = nums1[i--]
+    else if (nums1[i] >= nums2[j]) nums1[k--] = nums1[i--]
+    else nums1[k--] = nums2[j--]
+  }
+
+};
+// @lc code=end
+
