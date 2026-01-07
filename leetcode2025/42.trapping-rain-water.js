751dd0e7df4da1cae92909c932bc4a175bb6a989
pyuyu <122561112@qq.com>
2026-01-07 10:22:39 +0800

    [42] 接雨水

---
+++ leetcode2025/42.trapping-rain-water.js
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ * @lcpr version=30204
+ *
+ * [42] 接雨水
+ *
+ * https://leetcode.cn/problems/trapping-rain-water/description/
+ *
+ * algorithms
+ * Hard (64.83%)
+ * Likes:    6031
+ * Dislikes: 0
+ * Total Accepted:    1.6M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+ *
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * n == height.length
+ * 1 <= n <= 2 * 10^4
+ * 0 <= height[i] <= 10^5
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let res = 0;
+    let l = 0;
+    let r = height.length - 1;
+    const lmax = height[l];
+    const rmax = height[r]
+    while(l < r) {
+        lmax = Math.max(lmax, height[l]);
+        rmax = Math.max(rmax, height[r]);
+        if (lmax < rmax) {
+            res += lmax - height[l];
+            l++;
+        } else {
+            res += rmax - height[r];
+            r--;
+        }
+    }
+    return res;
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [0,1,0,2,1,0,1,3,2,1,2,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,2,0,3,2,5]\n
+// @lcpr case=end
+
+ */
+
