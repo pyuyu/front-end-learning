5d4c385f97e7b7ea2e148b270130d3218d25a1de
pyuyu <122561112@qq.com>
2026-01-11 11:24:16 +0800

    层序遍历

--- leetcode2025/104.maximum-depth-of-binary-tree.js
+++ leetcode2025/104.maximum-depth-of-binary-tree.js
@@ -64,11 +64,30 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    if(!root) return 0;
-    const left = maxDepth(root.left);
-    const right = maxDepth(root.right);
-    return Math.max(left, right) + 1;
+var maxDepth = function (root) {
+    // if(!root) return 0;
+    // const left = maxDepth(root.left);
+    // const right = maxDepth(root.right);
+    // return Math.max(left, right) + 1;
+
+    const q = [];
+    if (!root) return 0;
+    let res = 0
+    q.push(root);
+    while (q.length) {
+        let n = q.length;
+        res += 1;
+        while (n--) {
+            const node = q.shift();
+            if (node.left) {
+                q.push(node.left);
+            }
+            if (node.right) {
+                q.push(node.right);
+            }
+        }
+    }
+    return res;
 };
 // @lc code=end
 
