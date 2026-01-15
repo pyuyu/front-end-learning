43a8ae261ecebb64cbc54a078811d6dbb7f42b74
pyuyu <122561112@qq.com>
2026-01-15 21:54:41 +0800

    70. 198.

---
+++ leetcode2025/70.climbing-stairs.js
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=javascript
+ * @lcpr version=30204
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode.cn/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (55.55%)
+ * Likes:    3971
+ * Dislikes: 0
+ * Total Accepted:    2.1M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 
+ * 示例 2：
+ * 
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= n <= 45
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const dp = [];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n
+// @lcpr case=end
+
+ */
+
